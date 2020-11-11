a78d6acf18ed66033173810a113e1ab6aacaa649
Victorlerner <leonn366@gmail.com>
2020-11-11 12:48:33 +0200

    first check auto update plugin

--- definance-plugin.php
+++ definance-plugin.php
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 define( 'DEFINANCE_TEMPLATE_DIR', __DIR__ . '/templates' );
 define( 'DEFINANCE_BASE_DIR', __DIR__ );
 define( 'DEFINANCE_BASE_FILE', __FILE__ );
-
+define( 'DEFINANCE_VER', '1.0.0' );
 /**
  * Plugin Init
  */
