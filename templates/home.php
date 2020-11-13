eb4e6ffdb441bb976bccb660dc438688be4f44c6
Victorlerner <leonn366@gmail.com>
2020-11-13 17:42:29 +0200

    add viewport

--- templates/home.php
+++ templates/home.php
@@ -2,6 +2,8 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
+
 </head>
 <body <?php body_class() ?>
 >
