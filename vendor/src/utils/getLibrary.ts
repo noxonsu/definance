1cbcf216b3ff4cd07ee87b41884784591a436fcd
NotEternal <cent1pede@protonmail.ch>
2022-03-15 15:32:03 +0100

    Cleanup

--- vendor/src/utils/getLibrary.ts
+++
@@ -1,8 +0,0 @@
-"use strict"
-import { Web3Provider } from '@ethersproject/providers'
-
-export default function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider, 'any')
-  library.pollingInterval = 15000
-  return library
-}
