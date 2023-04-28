54b8c10d55dfcba5977ee294e7daa9754a0d1630
NotEternal <cent1pede@protonmail.ch>
2023-04-28 16:57:54 +0300

    build: update

--- vendor_source/service-worker.js
+++ vendor_source/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8ea8242ddc6c83f8bac6079ca88d230c.js"
+  "./precache-manifest.4d3e481519f864e05fadf57a308860e1.js"
 );
 
 self.addEventListener('message', (event) => {
