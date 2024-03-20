46ff7c5ed47fa9dc287260fd5807ee7a0db735a9
J94k <cent1pede@protonmail.ch>
2024-03-20 15:14:03 +0900

    chore: updat

--- vendor_source/service-worker.js
+++ vendor_source/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5718e9001056eff0c812cb3bf5999194.js"
+  "./precache-manifest.0cbe5d3e2f47c385c66fc7e06ae36d42.js"
 );
 
 self.addEventListener('message', (event) => {
